b7c215a62e64a8b772e81b6a3e8db9228e099cf2
mamath <15641879+Mamath2000@users.noreply.github.com>
2024-09-09 00:12:06 +0200

    step 1

--- loop.sh
+++ loop.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 DEVICE_IP=192.168.90.205
+DEVICE_NAME=desk_windows
 
 while true
 do
   echo main loop
   ping_check=`fping -4 -t 10 ${DEVICE_IP} &> /dev/null && echo success || echo fail`
 
-  echo $ping_check
-
   while [ ${ping_check} != "success" ]
   do
-   echo "."
+    # echo "."
 
-   ping_check=`fping -4 -t 10 ${DEVICE_IP} &> /dev/null && echo success || echo fail`
-   sleep 0.1
-   sleep 2
+    ping_check=`fping -4 -t 10 ${DEVICE_IP} &> /dev/null && echo success || echo fail`
+    sleep 0.1
+    # sleep 2
   done
 
   sleep 2
   echo "device connected"
-  timeout 10 python3 getconf.py ${DEVICE_IP}
+  # timeout 10 python3 getconf.py ${DEVICE_IP}
+  timeout 10 python3 getstate.py "${DEVICE_IP}" "${DEVICE_NAME}"
 
 done

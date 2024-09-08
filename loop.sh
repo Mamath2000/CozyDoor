7130d3f159b04b33a325cca0ffbe96a5654b3cb5
mamath <15641879+Mamath2000@users.noreply.github.com>
2024-09-08 20:10:49 +0200

    main

---
+++ loop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DEVICE_IP=192.168.90.205
+
+while true
+do
+  echo main loop
+  ping_check=`fping -4 -t 10 ${DEVICE_IP} &> /dev/null && echo success || echo fail`
+
+  echo $ping_check
+
+  while [ ${ping_check} != "success" ]
+  do
+   echo "."
+
+   ping_check=`fping -4 -t 10 ${DEVICE_IP} &> /dev/null && echo success || echo fail`
+   sleep 0.1
+   sleep 2
+  done
+
+  sleep 2
+  echo "device connected"
+  timeout 10 python3 getconf.py ${DEVICE_IP}
+
+done
